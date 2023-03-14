0e5c9a85ba3c5c54ecba62bd1265700d474436d6
SaranKaewchuay <101935507+SaranKaewchuay@users.noreply.github.com>
2023-03-14 14:09:34 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,8 +12,9 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("hello/{name}")
+@app.get("/hello/{name}")
 def read_name(name: str = None):
     return {"hello": name}
+
 
 handler = Mangum(app)
